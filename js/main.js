dac8242285ed22e1f5a54c8bbf65ae2ae5016823
yyt <839387491@qq.com>
2018-02-25 12:28:38 +0800

    navigator

--- js/main.js
+++ js/main.js
@@ -2,36 +2,36 @@
 	
 	'use strict';
 
-	// var isMobile = {
-	// 	Android: function() {
-	// 		return navigator.userAgent.match(/Android/i);
-	// 	},
-	// 		BlackBerry: function() {
-	// 		return navigator.userAgent.match(/BlackBerry/i);
-	// 	},
-	// 		iOS: function() {
-	// 		return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-	// 	},
-	// 		Opera: function() {
-	// 		return navigator.userAgent.match(/Opera Mini/i);
-	// 	},
-	// 		Windows: function() {
-	// 		return navigator.userAgent.match(/IEMobile/i);
-	// 	},
-	// 		any: function() {
-	// 		return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-	// 	}
-	// };
+	var isMobile = {
+		Android: function() {
+			return navigator.userAgent.match(/Android/i);
+		},
+			BlackBerry: function() {
+			return navigator.userAgent.match(/BlackBerry/i);
+		},
+			iOS: function() {
+			return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+		},
+			Opera: function() {
+			return navigator.userAgent.match(/Opera Mini/i);
+		},
+			Windows: function() {
+			return navigator.userAgent.match(/IEMobile/i);
+		},
+			any: function() {
+			return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+		}
+	};
 
 	
 	var fullHeight = function() {
 
-		// if ( !isMobile.any() ) {
+		if ( !isMobile.any() ) {
 			$('.js-fullheight').css('height', $(window).height());
 			$(window).resize(function(){
 				$('.js-fullheight').css('height', $(window).height());
 			});
-		// }
+		}
 	};
 
 	// Parallax
