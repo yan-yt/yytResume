937790708c23396c6c1f7b615f04aff417b15286
yyt <839387491@qq.com>
2018-03-08 19:52:43 +0800

    foemat

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
-;(function () {
-	
+; (function () {
+
 	'use strict';
 
 	//loading
 	$(document).ready(
-		document.onreadystatechange = function(){
-			if(document.readyState === "complete"){
+
+		document.onreadystatechange = function () {
+			if (document.readyState === "complete") {
 				$(".loadding").fadeOut("slow");
 			}
 		}
@@ -14,131 +15,128 @@
 
 
 	var isMobile = {
-		Android: function() {
+		Android: function () {
 			return navigator.userAgent.match(/Android/i);
 		},
-			BlackBerry: function() {
+		BlackBerry: function () {
 			return navigator.userAgent.match(/BlackBerry/i);
 		},
-			iOS: function() {
+		iOS: function () {
 			return navigator.userAgent.match(/iPhone|iPad|iPod/i);
 		},
-			Opera: function() {
+		Opera: function () {
 			return navigator.userAgent.match(/Opera Mini/i);
 		},
-			Windows: function() {
+		Windows: function () {
 			return navigator.userAgent.match(/IEMobile/i);
 		},
-			any: function() {
+		any: function () {
 			return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
 		}
 	};
 
 	// Parallax
-	var parallax = function() {
+	var parallax = function () {
 		$(window).stellar();//视差滚动
 	};
 
 
-	var pieChart = function() {
+	var pieChart = function () {
 		$('.chart').easyPieChart({
 			scaleColor: false,
 			lineWidth: 4,
 			lineCap: 'butt',
 			barColor: '#FF9000',
-			trackColor:	"#f5f5f5",
+			trackColor: "#f5f5f5",
 			size: 100,
 			animate: 1000
 		});
 	};
 
-	
-	// $(function(){
-		//滚动
-		var i = 0;
-		$('.animate-box').waypoint( function( direction ) {
 
-			if( direction === 'down' && !$(this.element).hasClass('animated-fast') ) {
-				
-				i++;
+	var i = 0;
+	$('.animate-box').waypoint(function (direction) {
 
-				$(this.element).addClass('item-animate');
-				setTimeout(function(){
+		if (direction === 'down' && !$(this.element).hasClass('animated-fast')) {
 
-					$('body .animate-box.item-animate').each(function(k){
-						var el = $(this);
-						setTimeout( function () {
-							var effect = el.data('animate-effect');
-							if ( effect === 'fadeIn') {
-								el.addClass('fadeIn animated-fast');
-							} else if ( effect === 'fadeInLeft') {
-								el.addClass('fadeInLeft animated-fast');
-							} else if ( effect === 'fadeInRight') {
-								el.addClass('fadeInRight animated-fast');
-							} else {
-								el.addClass('fadeInUp animated-fast');
-							}
+			i++;
 
-							el.removeClass('item-animate');
-						},  k * 50, 'easeInOutExpo' );
-					});
-					
-				}, 200);
-				
-			}
+			$(this.element).addClass('item-animate');
+			setTimeout(function () {
 
-		} , { offset: '85%' } );
-		//返回顶部
-		$('.js-gotop').on('click', function(event){
-			
-			event.preventDefault();
+				$('body .animate-box.item-animate').each(function (k) {
+					var el = $(this);
+					setTimeout(function () {
+						var effect = el.data('animate-effect');
+						if (effect === 'fadeIn') {
+							el.addClass('fadeIn animated-fast');
+						} else if (effect === 'fadeInLeft') {
+							el.addClass('fadeInLeft animated-fast');
+						} else if (effect === 'fadeInRight') {
+							el.addClass('fadeInRight animated-fast');
+						} else {
+							el.addClass('fadeInUp animated-fast');
+						}
 
-			$('html, body').animate({
-				scrollTop: $('html').offset().top
-			}, 500, 'easeInOutExpo');
-			
-			return false;
-		});
+						el.removeClass('item-animate');
+					}, k * 50, 'easeInOutExpo');
+				});
 
-		$(window).scroll(function(){
+			}, 200);
 
-			var $win = $(window);
-			if ($win.scrollTop() > 200) {
-				$('.js-top').addClass('active');
-			} else {
-				$('.js-top').removeClass('active');
-			}
+		}
 
-		});
-	
+	}, { offset: '85%' });
+	//返回顶部
+	$('.js-gotop').on('click', function (event) {
 
-		//判断是否占满整个屏幕
-		if ( !isMobile.any() ) {//判断是否是移动端
+		event.preventDefault();
+
+		$('html, body').animate({
+			scrollTop: $('html').offset().top
+		}, 500, 'easeInOutExpo');
+
+		return false;
+	});
+
+	$(window).scroll(function () {
+
+		var $win = $(window);
+		if ($win.scrollTop() > 200) {
+			$('.js-top').addClass('active');
+		} else {
+			$('.js-top').removeClass('active');
+		}
+
+	});
+
+
+	//判断是否占满整个屏幕
+	if (!isMobile.any()) {//判断是否是移动端
+		$('.js-fullheight').css('height', $(window).height());
+		$(window).resize(function () {
 			$('.js-fullheight').css('height', $(window).height());
-			$(window).resize(function(){
-				$('.js-fullheight').css('height', $(window).height());
-			});
-		}
-
-		//滑动
-		var $root = $('html, body');
-		$('#navbar-menu ul li a').click(function() {
-			$root.animate({
-				scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top - 100
-			}, 1000);
-			return false;
 		});
+	}
 
-		if ($('#resume-skills').length > 0 ) {
-			$('#resume-skills').waypoint( function( direction ) {
-										
-				if( direction === 'down' && !$(this.element).hasClass('animated') ) {
-					setTimeout( pieChart , 400);					
-					$(this.element).addClass('animated');
-				}
-			} , { offset: '90%' } );
-		}
-	// });
+	//滑动
+	var $root = $('html, body');
+	$('#navbar-menu ul li a').click(function () {
+		$root.animate({
+			scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top - 100
+		}, 1000);
+		return false;
+	});
+
+	if ($('#resume-skills').length > 0) {
+		$('#resume-skills').waypoint(function (direction) {
+
+			if (direction === 'down' && !$(this.element).hasClass('animated')) {
+				setTimeout(pieChart, 400);
+				$(this.element).addClass('animated');
+			}
+		}, { offset: '90%' });
+	}
 
 
 }());
